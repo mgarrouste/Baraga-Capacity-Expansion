5fb1f82b2124a257cdd12bbb253263da7e70abe0
Garrouste <mgarrou@UMICH.EDU>
2022-04-03 12:18:54 -0400

    Write cleaned up csv file for demand data to load into GAMS

--- ppMISOdata.py
+++ ppMISOdata.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import csv
 
 #print(os.getcwd())
 
@@ -15,11 +16,22 @@ def cleanUpData(path='./MISO.csv'):
     cleanMISOdata['Cleaned Demand (MW)'] = 1000*cleanMISOdata['Cleaned Demand (MW)']/max(cleanMISOdata['Cleaned Demand (MW)'])
     cleanMISOdata.rename(columns={'Cleaned Demand (MW)':'demand'}, inplace=True)
     # Select only 2018 data
-    print(cleanMISOdata.head())
-    mask = (cleanMISOdata['date_time']>'2018-01-01') & (cleanMISOdata['date_time']<'2018-12-31')
+    mask = (cleanMISOdata['date_time']>='2018-01-01') & (cleanMISOdata['date_time']<'2019-01-01')
     cleanMISOdata = cleanMISOdata.loc[mask]
-    print(cleanMISOdata.head())
     return cleanMISOdata['date_time'], cleanMISOdata['demand']
+
+def writeCleanCSV(time, demand):  
+    demand = list(demand)   
+    try: 
+        assert len(demand) == 8760
+    except AssertionError as e: 
+        print(e)
+        print(len(demand))
+    assert len(time) ==8760
+    with open('demand2018MISO1000MW.csv', mode='w', newline='') as file: 
+        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for t in range(1,8761):
+            writer.writerow(['t'+str(t), demand[t-1]])
 
 def plotDemand(time, demand): 
     fig, ax = plt.subplots(figsize=(10, 8))
@@ -42,4 +54,5 @@ if __name__ =="__main__":
     scriptPath = os.path.dirname(os.path.realpath(__file__))
     os.chdir(scriptPath)
     time,demand = cleanUpData()
+    writeCleanCSV(time, demand)
     plotDemand(time,demand)
