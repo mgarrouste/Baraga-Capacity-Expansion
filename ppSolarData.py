1e6aa61c139369b62662761b881e17a6d1acaadf
Garrouste <mgarrou@UMICH.EDU>
2022-04-03 12:05:52 -0400

    Write csv file with solar capacity data for GAMS

--- ppSolarData.py
+++ ppSolarData.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os 
 import matplotlib.dates as mdates 
 from dateutil import parser
+import csv
 
 
 def cleanUpData(path='./baraga_solar3.csv'):
@@ -12,6 +13,19 @@ def cleanUpData(path='./baraga_solar3.csv'):
     time = pd.to_datetime(solarData['Time stamp'])
     cf = solarData.iloc[:,-1].str.rstrip('%').astype(float)/100.0
     return time, cf
+
+def writeCleanCSV(time, cf):     
+    try: 
+        assert len(cf) == 8760
+    except AssertionError as e: 
+        print(e)
+        print(len(cf))
+    assert len(time) ==8760
+    with open('solarCFBaraga.csv', mode='w', newline='') as file: 
+        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for t in range(1,8761):
+            writer.writerow(['t'+str(t), cf[t-1]])
+
 
 def plotSolarCF(time, cf): 
     fig, ax = plt.subplots(figsize=(10, 8))
@@ -24,7 +38,7 @@ def plotSolarCF(time, cf):
     ax.set_title('Baraga county solar capacity factor')
     ax.grid()
     fig.autofmt_xdate()
-    plt.savefig('solarCF')
+    plt.savefig('solarCFBaraga')
     plt.close()
 
 if __name__ =="__main__":
@@ -33,3 +47,4 @@ if __name__ =="__main__":
     os.chdir(scriptPath)
     time, cf  = cleanUpData()
     plotSolarCF(time, cf)
+    writeCleanCSV(time,cf)
